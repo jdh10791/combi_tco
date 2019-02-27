fe28d26119d9d805a2e30ac7e3adc5a0f3265484
Jake Huang <jdh10791@gmail.com>
2019-02-27 14:10:44 -0700

    Added featurizers module

---
+++ scripts/helpers/featurizers.py
@@ -0,0 +1,332 @@
+# import os
+# os.chdir(os.path.join(os.environ['HOME'],"OneDrive - Colorado School of Mines/Research/MIDDMI/TCO/scripts"))
+
+import numpy as np
+import pandas as pd
+import pymatgen as mg
+import collections
+from pymatgen.core.composition import Composition
+from pymatgen.core.molecular_orbitals import MolecularOrbitals
+from .calc_chemfeat_2 import Perovskite
+from matminer.featurizers.conversions import StrToComposition
+from matminer.featurizers.composition import ElementProperty, ValenceOrbital, CohesiveEnergy
+from matminer.featurizers.base import BaseFeaturizer
+
+print('loaded featurizers')
+
+
+class AtomicOrbitalsMod(BaseFeaturizer):
+	"""
+	*Modified from matminer class to handle cases where LUMO is None*
+	Determine HOMO/LUMO features based on a composition.
+	The highest occupied molecular orbital (HOMO) and lowest unoccupied
+	molecular orbital (LUMO) are estiated from the atomic orbital energies
+	of the composition. The atomic orbital energies are from NIST:
+	https://www.nist.gov/pml/data/atomic-reference-data-electronic-structure-calculations
+	Warning:
+	For compositions with inter-species fractions greater than 10,000 (e.g.
+	dilute alloys such as FeC0.00001) the composition will be truncated (to Fe
+	in this example). In such extreme cases, the truncation likely reflects the
+	true physics of the situation (i.e. that the dilute element does not
+	significantly contribute orbital character to the band structure), but the
+	user should be aware of this behavior.
+	"""
+
+	def featurize(self, comp):
+		"""
+		Args:
+			comp: (Composition)
+				pymatgen Composition object
+		Returns:
+			HOMO_character: (str) orbital symbol ('s', 'p', 'd', or 'f')
+			HOMO_element: (str) symbol of element for HOMO
+			HOMO_energy: (float in eV) absolute energy of HOMO
+			LUMO_character: (str) orbital symbol ('s', 'p', 'd', or 'f')
+			LUMO_element: (str) symbol of element for LUMO
+			LUMO_energy: (float in eV) absolute energy of LUMO
+			gap_AO: (float in eV)
+				the estimated bandgap from HOMO and LUMO energeis
+		"""
+
+		integer_comp, factor = comp.get_integer_formula_and_factor()
+
+		# warning message if composition is dilute and truncated
+		if not (len(Composition(comp).elements) ==
+				len(Composition(integer_comp).elements)):
+			warn('AtomicOrbitals: {} truncated to {}'.format(comp,
+															 integer_comp))
+
+		homo_lumo = MolecularOrbitals(integer_comp).band_edges
+
+		feat = collections.OrderedDict()
+		
+		for edge in ['HOMO', 'LUMO']:
+			if homo_lumo[edge] is not None:
+				feat['{}_character'.format(edge)] = homo_lumo[edge][1][-1]
+				feat['{}_element'.format(edge)] = homo_lumo[edge][0]
+				feat['{}_energy'.format(edge)] = homo_lumo[edge][2]
+			else:
+				#if LUMO is None
+				feat['{}_character'.format(edge)] = 'na'
+				feat['{}_element'.format(edge)] = 'na'
+				feat['{}_energy'.format(edge)] = 0 #unclear what this value should be. Arbitrarily set to 0. Don't want NaN for modeling
+				
+		feat['gap_AO'] = feat['LUMO_energy'] - feat['HOMO_energy']
+
+		return list(feat.values())
+
+	def feature_labels(self):
+		feat = []
+		for edge in ['HOMO', 'LUMO']:
+			feat.extend(['{}_character'.format(edge),
+						 '{}_element'.format(edge),
+						 '{}_energy'.format(edge)])
+		feat.append("gap_AO")
+		return feat
+
+	def citations(self):
+		return [
+			"@article{PhysRevA.55.191,"
+			"title = {Local-density-functional calculations of the energy of atoms},"
+			"author = {Kotochigova, Svetlana and Levine, Zachary H. and Shirley, "
+			"Eric L. and Stiles, M. D. and Clark, Charles W.},"
+			"journal = {Phys. Rev. A}, volume = {55}, issue = {1}, pages = {191--199},"
+			"year = {1997}, month = {Jan}, publisher = {American Physical Society},"
+			"doi = {10.1103/PhysRevA.55.191}, "
+			"url = {https://link.aps.org/doi/10.1103/PhysRevA.55.191}}"]
+			
+	def implementors(self):
+		return ['Maxwell Dylla', 'Anubhav Jain']
+
+class PerovskiteProperty(BaseFeaturizer):
+	"""
+	Class to calculate perovskite features. Includes custom features from the Perovskite class and generic features from ElementProperty, 
+	AtomicOrbitals, ValenceOrbital, and CohesiveEnergy matminer featurizers.
+	
+	Can be initialized using from_preset() method
+	
+	Parameters:
+	-----------
+	cation_site: dict of site assignments for cations, i.e. {el:site}. Elements not in cation_site are assumed to be anions on X-site
+	site_ox_lim: dict of oxidation state limits for each site, i.e. {site:[min,max]}. Elements on sites are limited to oxidation states within these limits
+	site_base_ox: dict of base oxidation state for each site, i.e. {site:ox}. Used for determining aliovalent ions and acceptor/donor dopants
+	"""
+	def __init__(self, cation_site, site_ox_lim, site_base_ox):
+		self.cation_site = cation_site
+		self.site_ox_lim = site_ox_lim
+		self.site_base_ox = site_base_ox
+		
+		#matminer featurizers
+		self.ValenceOrbital = ValenceOrbital()
+		self.AtomicOrbitals = AtomicOrbitalsMod()
+		self.CohesiveEnergy = CohesiveEnergy()
+		#custom ElementProperty featurizer
+		elemental_properties = ['BoilingT', 'MeltingT',
+			'BulkModulus', 'ShearModulus', 
+			'Row', 'Column', 'Number', 'MendeleevNumber', 'SpaceGroupNumber',
+			'Density','MolarVolume',
+			'FusionEnthalpy','HeatVaporization',
+			'NsUnfilled','NpUnfilled','NdUnfilled','NfUnfilled',
+			'Polarizability', 
+			'ThermalConductivity']
+		self.ElementProperty = ElementProperty(data_source='magpie',features=elemental_properties,
+						  stats=["mean", "std_dev", "range"])
+		
+		self.check_matminer_featurizers()
+		self.featurize_options = {}
+	
+	@classmethod
+	def from_preset(cls, preset_name):
+		"""
+		Initialize from preset
+		
+		Parameters:
+		-----------
+		preset_name: name of preset to load. Currently accepts 'BCFZY'
+		"""
+		if preset_name=='BCFZY':
+			#Ba(Co,Fe,Zr,Y)O_3-d system
+			cation_site={'Ba':'A','Co':'B','Fe':'B','Zr':'B','Y':'B'}
+			site_ox_lim={'A':[2,2],'B':[2,4],'X':[-2,-2]}
+			site_base_ox={'A':2,'B':4,'X':-2}
+		else:
+			raise ValueError("Invalid preset_name specified!")
+		
+		return cls(cation_site,site_ox_lim, site_base_ox)
+		
+	@property
+	def ElementProperty_custom_labels(self):
+		"""
+		Generate custom labels for ElementProperty featurizer that follow same naming convention as Perovskite class
+		"""
+		elemental_property_label_map = {'BoilingT':'boil_temp','MeltingT':'melt_temp',
+							'BulkModulus':'bulk_mod','ShearModulus':'shear_mod',
+							'Row':'row','Column':'column','Number':'number','MendeleevNumber':'mendeleev','SpaceGroupNumber':'space_group',
+							'Density':'density','MolarVolume':'molar_vol',
+							'FusionEnthalpy':'H_fus','HeatVaporization':'H_vap',
+							'NsUnfilled':'valence_unfilled_s','NpUnfilled':'valence_unfilled_p','NdUnfilled':'valence_unfilled_d','NfUnfilled':'valence_unfilled_f',
+							'Polarizability':'polarizability',
+							'ThermalConductivity':'sigma_therm'}
+							
+		element_property_labels = list(map(elemental_property_label_map.get,self.ElementProperty.features))
+		labels = []
+		for attr in element_property_labels:
+			for stat in self.ElementProperty.stats:
+				if stat=='std_dev':
+					stat = 'std'
+				labels.append(f'{attr}_{stat}')
+		return labels
+		
+	def ElementProperty_label_check(self):
+		"""
+		Check that ElementProperty feature labels are as expected
+		If not, features may not align with feature labels
+		"""
+		#ElementProperty.feature_labels() code as of 2/17/19
+		labels = []
+		for attr in self.ElementProperty.features:
+			for stat in self.ElementProperty.stats:
+				labels.append("%s %s" % (stat, attr))
+		
+		if labels!=self.ElementProperty.feature_labels():
+			raise Exception('ElementProperty features or labels have changed')
+	
+	def set_featurize_options(self,sites,ox_stats=['min','max','mean','median','std','range'],ep_stats=["mean", "std_dev", "range"],radius_type='ionic_radius',normalize_formula=True,silent=True):
+		"""
+		Set options for featurization. Since these options change the number and meaning of features returned, safest to set for the whole class for consistency, 
+		rather than passing as inputs to each featurize() call
+		
+		Parameters:
+		-----------
+		sites: list or string of sites to featurize. Any combination of 'A', 'B', 'X', and/or 'comp' accepted. 
+			Composition-level, oxidation-state-dependent features are always calculated by the Perovskite class. Passing '' or [] will return only these features.
+			Specifying 'A','B', and/or 'X' sites will calculate site-level features for these sites (oxidation-state independent and dependent features, and matminer features).
+			Including 'comp' will calculate oxidation-state-independent features and matminer features for the full composition.
+		ox_stats: list of aggregate functions to apply to oxidation state combinations for feature generation using Perovskite class. 
+			Options: 'min','max','mean','median','std','range'
+		ep_stats: ElementProperty stats. Options: "minimum", "maximum", "range", "mean", "avg_dev", "mode"
+		radius_type: Shannon radius type to use in features. Accepts 'crystal_radius' or 'ionic_radius'
+		normalize_formula: if True, normalize formula such that higher occupancy cation site has one formula unit (applies to Perovskite class only)
+		silent: if False, print informational messages from Perovksite class
+		"""
+		
+		feat_options = dict(sites=sites,ox_stats=ox_stats,radius_type=radius_type,normalize_formula=normalize_formula,silent=silent)
+		self.featurize_options.update(feat_options)
+		self.ElementProperty.stats = ep_stats
+		
+	def featurize(self,formula):
+		"""
+		Calculate features
+		
+		Parameters:
+		-----------
+		formula: chemical formula string
+		
+		Returns: list of feature values
+		"""
+		try:
+			pvsk = Perovskite(formula, self.cation_site, self.site_ox_lim, self.site_base_ox,self.featurize_options['radius_type'],self.featurize_options['normalize_formula'],self.featurize_options['silent'])
+			pvsk_features = pvsk.featurize(self.featurize_options['sites'],self.featurize_options['ox_stats'])
+		except KeyError:
+			raise Exception('Featurize options have not been set. Use set_featurize_options before featurizing')
+			
+		mm_features = []
+		for site in self.featurize_options['sites']:
+				
+			vo_features = self.ValenceOrbital.featurize(pvsk.site_composition[site]) #avg and frac s, p , d, f electrons
+			vo_features += [sum(vo_features[0:3])] #avg total valence electrons
+			ao_features = self.AtomicOrbitals.featurize(pvsk.site_composition[site]) #HOMO and LUMO character and energy levels (from atomic orbitals)
+			ao_features = [ao_features[i] for i in range(len(ao_features)) if i not in (1,4)]#exclude HOMO_element and LUMO_element - categoricals
+			ce_features = self.CohesiveEnergy.featurize(pvsk.site_composition[site],formation_energy_per_atom=1e-10) #avg elemental cohesive energy
+			ep_features = self.ElementProperty.featurize(pvsk.site_composition[site]) #elemental property features
+			mm_features += vo_features + ao_features + ce_features + ep_features
+			
+		features = list(pvsk_features) + mm_features
+			
+		return features
+		
+	@property
+	def matminer_labels(self):
+		"""
+		Feature labels for matminer-derived features
+		"""
+		labels = [
+			#ValenceOrbital labels
+			'valence_elec_s_mean',
+			'valence_elec_p_mean',
+			'valence_elec_d_mean',
+			'valence_elec_f_mean',
+			'valence_elec_s_frac',
+			'valence_elec_p_frac',
+			'valence_elec_d_frac',
+			'valence_elec_f_frac',
+			'valence_elec_tot_mean',
+			#AtomicOrbitals labels
+			'HOMO_character',
+			'HOMO_energy',
+			'LUMO_character',
+			'LUMO_energy',
+			'AO_gap',
+			#CohesiveEnergy labels
+			'cohesive_energy_mean']
+			
+		#ElementProperty labels
+		labels += self.ElementProperty_custom_labels
+		
+		return labels
+	
+	def feature_labels(self):
+		"""
+		Get list of feature labels
+		"""
+		try:
+			pvsk_labels = Perovskite.from_preset('BaCoO3','BCFZY',silent=True).feature_labels(self.featurize_options['sites'],self.featurize_options['ox_stats'])
+		except KeyError:
+			raise Exception('Featurize options have not been set. Use set_featurize_options before accessing feature labels')
+		
+		mm_labels = []
+		for site in self.featurize_options['sites']:
+			if site=='comp':
+				site_label = 'comp'
+			else:
+				site_label = f'{site}site'	
+			mm_labels += [f'{site_label}_{label}' for label in self.matminer_labels]
+			
+		return pvsk_labels + mm_labels
+		
+	def check_matminer_featurizers(self):
+		"""
+		Check that features and feature order for matminer featurizers are as expected
+		If features or feature order have changed, featurize() may return unexpected features that do not align with feature_labels()
+		"""
+		#verify that matminer feature labels haven't changed
+		if self.ValenceOrbital.feature_labels() != ['avg s valence electrons',
+											 'avg p valence electrons',
+											 'avg d valence electrons',
+											 'avg f valence electrons',
+											 'frac s valence electrons',
+											 'frac p valence electrons',
+											 'frac d valence electrons',
+											 'frac f valence electrons']:
+			raise Exception('ValenceOrbital features or labels have changed')
+			
+		if self.AtomicOrbitals.feature_labels() != ['HOMO_character',
+											 'HOMO_element',
+											 'HOMO_energy',
+											 'LUMO_character',
+											 'LUMO_element',
+											 'LUMO_energy',
+											 'gap_AO']:
+			raise Exception('AtomicOrbitals features or labels have changed')
+
+		if self.CohesiveEnergy.feature_labels() != ['cohesive energy']:
+			raise Exception('CohesiveEnergy features or labels have changed')
+											 
+		self.ElementProperty_label_check()
+		
+	# def matminer_featurizer_labels(self):
+		# #verify that matminer feature labels haven't changed
+		# if self.ValenceOrbital.feature_labels()[0:3]=
+		
+	
+	
